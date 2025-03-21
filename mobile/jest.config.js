c81bfbdbc29ba1c9a4ca27752c2497e00e100977
Robert Eriksen <rlerikse@mtu.edu>
2025-03-21 02:21:05 -0400

    Update mobile deployment

---
+++ mobile/jest.config.js
@@ -0,0 +1,12 @@
+/// <reference types="node" />
+module.exports = {
+  preset: 'react-native',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|@react-navigation|expo|@expo|@unimodules|unimodules|sentry-expo|native-base|react-native-svg)/)',
+  ],
+  setupFiles: ['<rootDir>/node_modules/react-native/jest/setup.js'],
+};
